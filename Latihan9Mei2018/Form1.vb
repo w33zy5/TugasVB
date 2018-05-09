b4bc2aeef49a57bc6d2c36545a5831f8a241d428
I Dewa Gede Agung Krisnayana <dewaagung44@gmail.com>
2018-05-09 09:52:07 +0800

    Binding Data Lesson

---
+++ Latihan9Mei2018/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Dim xConn As New koneksi
+    Dim xTabel As DataTable
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CreateTable()
+    End Sub
+
+    Function CreateTable()
+        xTabel = New DataTable
+        xTabel.Columns.Add("id", CStr("0").GetType)
+        xTabel.Columns.Add("nama", CStr("0").GetType)
+        xTabel.Columns.Add("alamat", CStr("0").GetType)
+
+        Dim xRow As DataRow = xTabel.NewRow
+        xRow(0) = "1"
+        xRow(1) = "Adi"
+        xRow(2) = "Budi"
+        xTabel.Rows.Add(xRow)
+
+        DataGridView1.DataSource = xTabel
+        DataGridView1.Refresh()
+
+    End Function
+
+End Class
